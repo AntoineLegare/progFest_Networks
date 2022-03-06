3e60f9540b958bff51c86854205820e216032253
Antoine Légaré <39058840+AntoineLegare@users.noreply.github.com>
2022-03-06 14:31:59 -0500

    Add files via upload

--- animateEpidemic.py
+++ animateEpidemic.py
@@ -22,27 +22,22 @@ class Epidemic:
         status = np.zeros((self.N, T)).astype('int')
         status[:, 0] = self.status[:, -1]
         for i in range(1, T):
-            recoveries = np.random.uniform(0, 1, self.N) > (1 - p_recovery)
-            status[recoveries, i] = 0
-            infectionProbabilities = p_infection * (self.A @ status[:, i - 1])
-            infections = np.random.uniform(0, 1, self.N) < infectionProbabilities
+            infectedPeople = status[:, i - 1] == 1
+            status[infectedPeople, i] = 1
+            infectedContacts = p_infection * (self.A @ status[:, i - 1])
+            immuneResponse = np.random.uniform(0, 1, self.N)
+            infections = infectedContacts > immuneResponse
             status[infections, i] = 1
+            condition1 = np.random.uniform(0, 1, self.N) > (1 - p_recovery)
+            condition2 = (status[:, i - 1] == 1)
+            recoveries = condition1 & condition2
+            status[recoveries, i] = 0
         self.status = np.append(self.status, status[:, 1:], axis=1)
-
 
 def WSNetwork(N, k, p):
     G = nx.watts_strogatz_graph(N, k, p)
     matrix = nx.to_numpy_array(G)
     return matrix
-
-
-def randomNetwork(N, rho):
-    G = nx.erdos_renyi_graph(N, rho)
-    weightedMatrix = nx.to_numpy_array(G)
-    IDs = np.where(weightedMatrix == 1)
-    for i in range(len(IDs[0])):
-        weightedMatrix[IDs[0][i], IDs[1][i]] = np.random.normal(0, (1 / ((N * rho) ** (1/3))))
-    return weightedMatrix
 
 
 def modularNetwork(moduleSizes, p_intra, p_inter):
@@ -59,17 +54,7 @@ def modularNetwork(moduleSizes, p_intra, p_inter):
     return matrix
 
 
-def BANetwork(N, M):
-    G = nx.barabasi_albert_graph(N, M)
-    weightedMatrix = nx.to_numpy_array(G)
-    IDs = np.where(weightedMatrix == 1)
-    rho = len(IDs[0]) / ((N ** 2) - N)
-    for i in range(len(IDs[0])):
-        weightedMatrix[IDs[0][i], IDs[1][i]] = np.random.normal(0, (1 / ((N * rho) ** (1/3))))
-    return weightedMatrix
-
-
-def animateEpidemic(epidemic, Nframes, step, nodeSize=50):
+def animateEpidemic(epidemic, Nframes, fps=5, nodeSize=50):
 
     G = nx.from_numpy_array(np.abs(epidemic.A))
     pos = nx.spring_layout(G)  # positions for all nodes
@@ -83,36 +68,38 @@ def animateEpidemic(epidemic, Nframes, step, nodeSize=50):
     plt.tight_layout(pad=0)
 
     def drawframe(i):
-        nodes = nx.draw_networkx_nodes(G, pos, node_size=nodeSize, node_color=epidemic.status[:, i * step], cmap='Reds',
+        nodes = nx.draw_networkx_nodes(G, pos, node_size=nodeSize, node_color=epidemic.status[:, i], cmap='Reds',
                                        vmin=0, vmax=1)
-        title.set_text(r"$t = {:.0f}$".format(i * step))
+        title.set_text(r"$t = {:.0f}$".format(i))
         return nodes, title,
 
-    anim = animation.FuncAnimation(fig, drawframe, frames=Nframes, interval=50, blit=True)
+    anim = animation.FuncAnimation(fig, drawframe, frames=Nframes, interval=int(1000/fps), blit=True)
     plt.show()
 
 
 if __name__ == '__main__':
 
-    N = 1000  # People
-    I_0 = 2 # Initial number of infected people
-    p_infection = 0.01
+    N = 500  # People
+    I_0 = 50 # Initial number of infected people
+    p_infection = 0.017
     p_recovery = 0.1
+    duration = 1000
+    fps = 30
 
-    #adjacencyMatrix = WSNetwork(N, 20, 0.05)
-    adjacencyMatrix = modularNetwork([int(N/2)] + [int(N/10)] * 5, [0.75] + [0.75] * 5, 0.01)
-    # adjacencyMatrix = randomNetwork(N, 0.1)
-    # adjacencyMatrix = BANetwork(N, 20)
+    adjacencyMatrix = WSNetwork(N, 10, 0.2)
+    # adjacencyMatrix = modularNetwork([int(N/2)] + [int(N/10)] * 5, [0.05] + [0.1] * 5, 0.005)
 
     epidemic = Epidemic(N, I_0=I_0)
     epidemic.initiateContacts(adjacencyMatrix)
-    epidemic.simulate(1000, p_infection=p_infection, p_recovery=p_recovery)
+    epidemic.simulate(duration, p_infection=p_infection, p_recovery=p_recovery)
 
-    animateEpidemic(epidemic, 1000, 1, nodeSize=50)
+    animateEpidemic(epidemic, duration, fps, nodeSize=50)
 
     plt.figure(figsize=(7, 5))
     plt.plot(np.mean(epidemic.status, axis=0), linewidth=2, color='black')
     plt.ylabel('Fraction of infected people')
     plt.xlabel('Time steps')
     plt.tight_layout(pad=0)
+    plt.ylim([0, 1])
+    plt.xlim([0, duration])
     plt.show()
